f5189e6825677d3c4d6123c2f9d70c09dc058f9a
FransErasmus <fransh.erasmus@gmail.com>
2021-08-12 08:50:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ng-upgrade-downgrade-xcxaen
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ng-upgrade-downgrade-xcxaen)
---
+++ src/angularjs/user/user.module.ajs.ts
@@ -0,0 +1,7 @@
+import angular from 'angular';
+
+import { ajsUserComponent } from './user.component.ajs';
+
+export const angularjsUserModule = 'ajs-user-module';
+
+angular.module(angularjsUserModule, []).component(...ajsUserComponent);
